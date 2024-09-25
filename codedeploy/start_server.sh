81e6f0c0a9bc67e3fec68e65c922fd9de402d02a
unknown <minhducproperty@gmail.com>
2024-09-25 21:51:13 +0700

    - deploy
    Signedoff-by: unknown <minhducproperty@gmail.com>

--- codedeploy/start_server.sh
+++ codedeploy/start_server.sh
@@ -2,5 +2,6 @@
 # source /home/ec2-user/web/.env
 
 # $(aws ecr get-login --no-include-email --region $REGION)
+aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 014498620594.dkr.ecr.ap-southeast-1.amazonaws.com
 
 cd /home/ec2-user/web/ && /usr/local/bin/docker-compose -f docker-compose.yml up -d
